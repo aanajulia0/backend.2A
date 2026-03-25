252efb0d59fb5447d6865cfe12747c05c2d06772
aanajulia0 <a.ana20@escola.pr.gov.br>
2026-03-25 18:29:09 +0000

    v1

---
+++ produto.js
@@ -0,0 +1,9 @@
+const produto = "mochila impermeavél";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessório";
+
+console.log( `O produto é: ${produto}.`);
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`O estoque ainda possui ${estoque} mochilas. `);
+console.log( `A categiria de ${produto} é ${categoria}. `);

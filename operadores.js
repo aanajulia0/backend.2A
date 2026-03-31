3e381b4b4de6af2ce51972d6916e3e868025b4e9
aanajulia0 <a.ana20@escola.pr.gov.br>
2026-03-31 17:36:16 +0000

    v1

---
+++ operadores.js
@@ -0,0 +1,28 @@
+//operador E &&
+let maiorIdade = true;
+let possuiDocumento = true;
+
+if (maiorIdade && possuiDocumento) {
+    console.log ("pode entrar na festa!");
+} else {
+    console.log ("Não pode entrar na festa.")
+};
+
+// operador OU ||
+let cartaoCredito = false;
+let dinheiro = false;
+
+if (cartaoCredito || dinheiro) {
+    console.log ("pode fazer a compra. ")
+} else {
+    console.log ("Não pode fazer a compra. ")
+};
+
+// operdador NOT !
+let estaChovendo = true;
+
+if (estaChovendo) {
+    console.log ("Pode sair sem guarda-chuva. ")
+} else {
+    console.log ('leve o guarda-chuva')
+};

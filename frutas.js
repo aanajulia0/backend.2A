4ebc2c7707e51ac790035ad630cb08f8223fdfa7
aanajulia0 <a.ana20@escola.pr.gov.br>
2026-05-05 16:48:27 +0000

    v2

--- frutas.js
+++ frutas.js
@@ -1,9 +1,20 @@
 //Lista ordenada de valores
 
-const frutas = ["Maçã", "Banana", "Uva", "Manga"]
+const frutas = ["Maçã","Banana", "uva", "Manga"];
+
 
 console.log(frutas);
-console.log(frutas[0]);
+console.log (frutas[0]);
 
 frutas[0]="Abacaxi"
-console.log(frutas)
+console.log(frutas);
+
+frutas[2]="Laranja"
+frutas[3]="Pera"
+console.log(frutas);
+console.log(frutas.length)
+frutas.push("pessego");
+console.log(frutas);
+
+console.log(frutas.indexOf("pessego"));
+console.log(frutas.indexOf("Laranja"));

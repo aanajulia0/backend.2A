b45da641332bf18b772cc6d3d3f7e061a0910366
aanajulia0 <a.ana20@escola.pr.gov.br>
2026-05-11 19:10:52 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+console.log ("perfil do jogador")
+
+let nomeJogador = "Neymar";
+let posicao = "Atacante";
+let idadeJogador = "34 anos";
+let time = "Barcelona";
+let gols = "450";
+
+console.log(`O jogador ${nomeJogador} joga como ${posicao}, e tem ${idadeJogador}. Atualmente está no ${time}, e ja fez mais de ${gols} gols.`);
